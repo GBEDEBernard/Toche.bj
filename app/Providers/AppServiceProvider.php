d8925c3b63e567460dbdb767946e26fe3280c37e
GBEDEBernard <gbedebernard60@gmail.com>
2025-10-20 15:35:00 +0100

    changer le providers pour obliges le site à utilisé https au lieu de http: remove key generation from build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot()
+{
+    // vient du fait que tu utilises http:// au lieu de https://.  correction pour forcer à chaque formulaire
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
+}
    
 }
