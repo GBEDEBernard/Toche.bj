06bfea17bb7b2431e1d72916e619f5a69330975c
GBEDE Bernard <gbedebernard60@gmail.com>
2025-10-21 09:48:35 +0100

    Insert debug statement for authenticated user
    Added debugging output for authenticated user.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
-        <h1>Bienvenue</h1>
-        <p>Utilisateur : {{ auth()->user()->name }}</p>
-        <p>Rôle : {{ auth()->user()->getRoleNames()->join(', ') }}</p>
-    </div>
+
+   @php
+    dd(auth()->user());
+@endphp
+
 @endsection
